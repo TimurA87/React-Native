00027c94fad7fc735c822fd6b2207c6b67d5b3af
timur <timuradev@gmail.com>
2021-05-19 18:28:58 +0300

    lessons 155-169

--- MealsToGo/src/features/restaurants/screens/restaturants-screen.js
+++ MealsToGo/src/features/restaurants/screens/restaturants-screen.js
@@ -1,16 +1,13 @@
 import React, { useContext } from "react";
 import { FlatList } from "react-native";
 import styled from "styled-components/native";
-import { Searchbar, ActivityIndicator, Colors } from "react-native-paper";
+import { ActivityIndicator, Colors } from "react-native-paper";
 
 import { RestaurantInfoCard } from "../compnents/restaurant-info-card.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
-
-const SearchContainer = styled.View`
-  padding: ${(props) => props.theme.space[3]};
-`;
+import { Search } from "../compnents/search.component";
 
 const RestarauntList = styled(FlatList).attrs({
   contentContainerStyle: {
@@ -18,32 +15,37 @@ const RestarauntList = styled(FlatList).attrs({
   },
 })``;
 
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
 export const RestaurantScreens = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
   return (
     <SafeArea>
-      <SearchContainer>
-        <Searchbar />
-      </SearchContainer>
-      {isLoading ?
-        <ActivityIndicator
-          size="large"
-          animating={true}
-          color={Colors.red800}
-        />
-        :
-        <RestarauntList
-          data={restaurants}
-          renderItem={({ item }) => {
-            return (
-              <Spacer position="bottom" size="large">
-                <RestaurantInfoCard restaraunt={item} />
-              </Spacer>
-            );
-          }}
-          keyExtractor={(item) => item.name}
-        />
-      }
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animation={true} color={Colors.blue300} />
+        </LoadingContainer>
+      )}
+      <Search />
+      <RestarauntList
+        data={restaurants}
+        renderItem={({ item }) => {
+          return (
+            <Spacer position="bottom" size="large">
+              <RestaurantInfoCard restaraunt={item} />
+            </Spacer>
+          );
+        }}
+        keyExtractor={(item) => item.name}
+      />
     </SafeArea>
   );
 };
---
+++ MealsToGo/src/services/location/location.context.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import { LocationRequest, LocationTransform } from "./location.service";
+
+export const LocationContext = React.createContext();
+
+export const LocationContextProvider = ({ children }) => {
+  const [location, setLocation] = useState([]);
+  const [keyword, setKeyword] = useState("San Francisco");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const onSearch = (searchKeyword) => {
+    setIsLoading(true);
+    setKeyword(searchKeyword);
+  };
+
+  useEffect(() => {
+    if (!keyword.length) {
+      return;
+    }
+    LocationRequest(keyword.toLowerCase())
+      .then(LocationTransform)
+      .then((result) => {
+        setIsLoading(false);
+        setLocation(result);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(err);
+      });
+  }, [keyword]);
+
+  return (
+    <LocationContext.Provider
+      value={{
+        isLoading,
+        error,
+        location,
+        search: onSearch,
+        keyword,
+      }}
+    >
+      {children}
+    </LocationContext.Provider>
+  );
+};
